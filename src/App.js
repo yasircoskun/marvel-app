5743d3c638ae94a77e4b1d315e12335d58e5c0a7
yasircoskun <yasir.coskun@outlook.com>
2022-03-09 08:25:03 +0300

    api key problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import axios from 'axios';
 import CharacterList from './components/CharacterList';
 import './App.scss'
 import config from './apikey';
@@ -8,40 +7,18 @@ import config from './apikey';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { charaters: [], offset: 0 }
-  }
-
-  async getCharacters() {
-    const response = await axios.get('https://gateway.marvel.com:443/v1/public/characters', {
-      params: {
-        offset: this.state.offset,
-        limit: 30,
-        apikey: window.apikey
-      }
-    });
-
-    this.setState({ charaters: [...response.data.data.results], offset: this.state.offset += 30 });
-    console.log(this.state.charaters)
+    this.state = {}
   }
 
   async componentDidMount() {
     window.apikey = config.apikey;
     window.apisecret = config.apisecret;
-    this.getCharacters()
-    var that = this;
-    // Scroll Bottom Detection
-    window.onscroll = async function (ev) {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        that.getCharacters();
-        window.scrollTo(0, 0);
-      }
-    };
   }
 
   render() {
     return (
       <div className='Center'>
-        <CharacterList charaters={this.state.charaters} />
+        <CharacterList />
       </div>
     );
   }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
+import axios from 'axios';
+
 
 class CharacterList extends React.Component {
   constructor(props) {
     super(props);
     this.setApiKey = this.setApiKey.bind(this);
+    this.state = { reRender: 0, charaters: [], offset: 0};
   }
 
   setApiKey(){
     window.apikey = document.getElementById('apikeyInput').value
     window.apisecret = document.getElementById('apisecretInput').value
+    this.setState({reRender: this.state.reRender + 1});
+    this.getCharacters()
+  }
+
+  async getCharacters() {
+    const response = await axios.get('https://gateway.marvel.com:443/v1/public/characters', {
+      params: {
+        offset: this.state.offset,
+        limit: 30,
+        apikey: window.apikey
+      }
+    });
+
+    this.setState({ charaters: [...response.data.data.results], offset: this.state.offset += 30 });
+    console.log(this.state.charaters)
+  }
+
+
+  async componentDidMount() {
+    this.getCharacters()
+    var that = this;
+    // Scroll Bottom Detection
+    window.onscroll = async function (ev) {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        that.getCharacters();
+        window.scrollTo(0, 0);
+      }
+    };
   }
 
   render() {
-    console.log(this.props.charaters);
-    if (this.props.charaters.length > 0) {
+    if (this.state.charaters.length > 0) {
       return (
-        <div className='CharacterList'>
-          {this.props.charaters.map(charater => {
+        <div renderNum={this.state.reRender} className='CharacterList'>
+          {this.state.charaters.map(charater => {
             return (
               <CharacterCard
                 key={charater.id}
@@ -41,7 +71,7 @@ class CharacterList extends React.Component {
       <br></br>
       <input id="apisecretInput" type="text" placeholder='My API Secret Key'></input>
       <br></br>
-      <button onClick={this.setApiKey}>Use My API Key</button>
+      <button onClick={this.setApiKey.bind(this)}>Use My API Key</button>
     </div>);
   }
 }
